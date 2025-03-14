39e9b470f8dcef99a0f4de394751eb15ec75cb9c
jzlpr <jazzlikeproblem@gmail.com>
2025-03-14 14:03:54 +0100

    Backup script

---
+++ backup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# backup.sh
+#
+
+# Files to backup
+backup_files="/home/$USER/sources/repos"
+
+dest="/home/$USER"
+
+# Create archive filename.
+day=$(date +%A)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+echo
+    
+# Backup the files using tar.
+tar czf $dest/$archive_file $backup_files
+    
+# Print end status message.
+echo
+echo "Backup finished"
+date
+    
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest
